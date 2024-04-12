f966a903685138263175a9b9021d0887e73d253c
준희 <ht0729@gachon.ac.kr>
2024-04-12 12:38:26 +0900

    test

---
+++ src/main/java/kubernetes_tset/kubernetes_tset/KubernetesTsetApplication.java
@@ -0,0 +1,13 @@
+package kubernetes_tset.kubernetes_tset;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KubernetesTsetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KubernetesTsetApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kubernetes_tset/kubernetes_tset/KubernetesTsetApplicationTests.java
@@ -0,0 +1,13 @@
+package kubernetes_tset.kubernetes_tset;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KubernetesTsetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
